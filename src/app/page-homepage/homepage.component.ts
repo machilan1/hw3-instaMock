64eefc9008482d3e4b9489923de710d3921fe997
machilan1 <machilan1@gmail.com>
2023-03-07 09:39:32 +0800

    getting better

--- src/app/page-homepage/homepage.component.ts
+++ src/app/page-homepage/homepage.component.ts
@@ -2,12 +2,12 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CommentPageComponent } from './comment-page/comment-page.component';
 import { PostComponent } from './post/post.component';
-import { Subject, Subscription, takeUntil } from 'rxjs';
 import { PostService } from './component-services/post.service';
+import { Observable, Subject, Subscription, takeUntil } from 'rxjs';
 import { Post } from './component-models/post.model';
+import { User } from '../data-user/users/user.model';
 import { ClientService } from '../data-user/client/client.service';
-import { UserService } from '../data-user/users/users.service';
-import { Router } from '@angular/router';
+import { HomeStatusService } from './component-services/home-status.services';
 @Component({
   selector: 'app-homepage',
   standalone: true,
@@ -15,11 +15,39 @@ import { Router } from '@angular/router';
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.scss'],
 })
-export class HomepageComponent   {
+export class HomepageComponent implements OnInit, OnDestroy {
   constructor(
+    private postService: PostService,
+    private clientService: ClientService,
+    private homeStatusService:HomeStatusService
+    ) {}
 
-  ) {}
+  // esse
+  active!:boolean;
+  posts$!: Observable<Post[]>;
+  clientID$!: Observable<string>;
+  commentPageActive$!:Observable<boolean>
+  //subs
+  activeSub!:Subscription;
+  //
+    destroy$ = new Subject<boolean>;
 
+  ngOnInit(): void {
+    this.posts$ = this.postService.posts$
+    this.clientID$ = this.clientService.currentClientID$
 
+    this.activeSub = this.homeStatusService.commentPageActive$
+    .pipe(
+      takeUntil(this.destroy$)
+    )
+    .subscribe(x=>this.active=x)
+  }
 
+  ngOnDestroy(): void {
+    this.destroy$.next(true)
+  }
+
+  onCheckMessage(){
+    
+  }
 }
